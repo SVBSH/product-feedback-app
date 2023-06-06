443e32bc417a8ae63974c26c5a06c2f0933d858f
SVBSH <getenv@protonmail.com>
2023-06-06 15:22:25 +0200

    update

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,12 @@ const DB_URI = process.env.PRODUCT_FEEDBACK_APP_DB_URI;
 const DB_COLLECTION = process.env.PRODUCT_FEEDBACK_APP_NS;
 const PORT = process.env.SERVER_PORT || 9000
 
+app.listen(PORT);
 
-MongoClient.connect(DB_URI).then(
-  async client => {
-    ProductRequestsDAO.injectDB(client);
-    app.listen(PORT);
-  }
-)
+
+// MongoClient.connect(DB_URI).then(
+//   async client => {
+//     ProductRequestsDAO.injectDB(client);
+//     app.listen(PORT);
+//   }
+// )
