5aa45eff1a5ce95ec36e3b824be0a4ae294779c6
SVBSH <getenv@protonmail.com>
2023-06-07 19:34:53 +0200

    update

---
+++ frontend/functions/get_movies.js
@@ -0,0 +1,27 @@
+const { MongoClient } = require("mongodb");
+require('dotenv').config();
+
+const mongoClient = new MongoClient(process.env.MONGODB_URI,
+  { 
+    useNewUrlParser: true ,
+    useUnifiedTopology: true,
+  }
+)
+
+const clientPromise = mongoClient.connect();
+
+const handler = async (event) => {
+  try {
+    const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
+    const collection = database.collection(process.env.MONGODB_COLLECTION);
+    const results = await collection.find({}).limit(10).toArray();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(results),
+    }
+  } catch (e) {
+    return { statusCode: 500, body: error.toString() + "lala" };
+  }
+}
+// https://explorers.netlify.com/learn/up-and-running-with-serverless-functions/introduction-with-serverless-functions
+module.exports = { handler };
